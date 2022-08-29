49df1ccd4c52eb524656a1901ac5dd68726e40e0
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-29 20:02:17 +0300

    get number of notification

--- lib/services/get_num_notification.dart
+++ lib/services/get_num_notification.dart
@@ -1,10 +1,59 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controller/controller.dart';
 import '../models/reminder.dart';
 
-getLastNotificationDataFun() async {
-  RxList<Reminder> notificationTasks = await ReminderController().getTasks();
-  print(notificationTasks.length);
-  return notificationTasks;
+class Notification extends StatefulWidget {
+  const Notification({Key? key}) : super(key: key);
+
+  @override
+  State<Notification> createState() => _NotificationState();
+}
+
+class _NotificationState extends State<Notification> {
+  RxList<Reminder> notificationTaskList = <Reminder>[].obs;
+  Timer? timer;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    timer = Timer.periodic(
+        Duration(seconds: 2), (Timer t) => getLastNotificationData());
+  }
+
+  getLastNotificationData() async {
+    RxList<Reminder> tasks = await ReminderController().getTasks();
+    print('notificationTaskList.length = ${notificationTaskList.length}');
+    setState(() => notificationTaskList = tasks);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Stack(
+      alignment: Alignment.topRight,
+      children: [
+        const Icon(Icons.notifications_rounded),
+        notificationTaskList.length == 0
+            ? Container()
+            : SizedBox(
+                width: 15,
+                height: 15,
+                child: CircleAvatar(
+                  backgroundColor: Colors.red,
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Text(
+                      notificationTaskList.length.toString(),
+                      style: TextStyle(color: Colors.white, fontSize: 9),
+                    ),
+                  ),
+                ),
+              ),
+      ],
+    );
+  }
 }
