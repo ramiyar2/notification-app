0f8c8d19cb34b5989744cbfd38017d7f246d4b6d
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-09-12 23:46:18 +0300

    main

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:notification_app/db/db.dart';
+import 'package:notification_app/services/bg_task_manager.dart';
+import 'package:notification_app/ui/home_page.dart';
+import 'package:workmanager/workmanager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HelperDb.initDb();
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(
+        backgroundColor: Colors.teal,
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Notifiction Demo',
+      home: const HomePage(),
+    );
+  }
+}
